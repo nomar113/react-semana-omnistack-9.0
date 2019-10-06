81ae20cfd42962fe1d1e599d6d030407035b1ee9
Ramon Mesquita <ramonmesquita113@gmail.com>
2019-10-06 11:15:21 -0300

    Esqueleto dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import api from '../../services/api'
 
-export default function Dashboard() {
+export default function Dashboard () {
+    const [spots, setSpots] = useState([])
+
+    // useEffect(() => {},[]) : carrega a informação assim que acessa a página
+    // [] : array de dependência, toda vez que esse valor alterar executa a função 
+    // array vazio significa assim que carregar
+
+    useEffect(() => {
+        async function loadSpots () {
+            const user_id = localStorage.getItem('user')
+            const response = await api.get('/dashboard', { headers: { user_id } })
+
+            setSpots(response.data)
+        }
+
+        loadSpots()
+    }, [])
+
     return (
-        <h1>Dashboard</h1>
+        <>
+            <ul className="spot-list">
+                {spots.map(spot => (
+                    <li key={spot._id}>
+                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />
+                        <strong>{spot.company}</strong>
+                        <span>{spot.price}</span>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
