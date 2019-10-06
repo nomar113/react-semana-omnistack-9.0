5e83e2601b77f970c523caaa34746fdc48a183b3
Ramon Mesquita <ramonmesquita113@gmail.com>
2019-10-06 07:06:52 -0300

    Estilização inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from './assets/logo.svg'
+
+function App () {
+    return (
+        <div className="container">
+            <img src={logo} alt="AirCnC" />
+
+            <div className="content">
+                <p>
+                    Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para usa empresa.
+                </p>
+
+                <form >
+                    <label htmlFor="email">E-MAIL *</label>
+                    <input
+                        id="email"
+                        type="email"
+                        placeholder="Seu melhor e-mail"
+                    />
+
+                    <button className="btn" type="submit">Entrar</button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default App;
